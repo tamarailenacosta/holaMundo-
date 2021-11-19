612a817c3bfdd18839a61066b9bf5b71f3cd5a64
Tamara Acosta <tamara.ailen.acosta@gmail.com>
2021-11-19 16:21:15 +0100

    agrego delete endpoint

--- models/cartModel.js
+++ models/cartModel.js
@@ -26,14 +26,34 @@ class CartModel {
 
     }
 
+    getById = async(idCart) => {
+        try {
+            const cart = await this.getAll()
+            const mycart = cart.find(p => p.id === idCart)
+            if (mycart === undefined) {
+                return null;
+            }
+            return mycart
+
+        } catch (err) {
+            console.log('error de lectura de achivo cart')
+            throw err
+
+        }
+
+    }
+
     //saveById 
     save = async() => {
 
         try {
             const all = await this.getAll()
+            let cart = {}
             cart.id = all.length + 1
             cart.timestamp = Date.now()
+            cart.productos = {}
             all.push(cart)
+            console.log(all)
             fs.promises.writeFile(this.ruta, JSON.stringify(all))
             return cart.id
         } catch (err) {
@@ -43,12 +63,55 @@ class CartModel {
 
     }
 
+    addProduct = async(product, ID) => {
+
+        try {
+            const cart = await this.getAll()
+            let idcart = parseInt(ID)
+            const mycart = cart.find(p => p.id === idcart)
+            console.log(mycart)
+            if (mycart === undefined) {
+                return null;
+            }
+            mycart.productos = product
+                // aca tengo que remplazar el cart 2 por un nuevo cart, sino se duplica
+            cart.push(mycart)
+            console.log(cart)
+            await fs.promises.writeFile(this.ruta, JSON.stringify(cart))
+            return true
+        } catch (err) {
+            console.log("error al escribir el archivo cart.txt")
+            throw err
+        }
+
+    }
+
+
+    deleteProduct = async(idProd, myCart) => {
+
+        try {
+            const found = myCart.forEach(element => console.log(element))
+                /*cart.push(mycart)
+                console.log(cart)
+                await fs.promises.writeFile(this.ruta, JSON.stringify(cart))
+                return true*/
+        } catch (err) {
+            console.log("error al escribir el archivo cart.txt")
+            throw err
+        }
+
+    }
+
     //deleteById 
     delete = async(ID) => {
-        let id = parseInt(ID)
+        let id = ID
         try {
-            const all = await this.getAll(id);
-            const filtered = all.filter(c => c.id !== id);
+            const all = await this.getAll();
+            const found = all.find(c => c.id === id)
+            if (found === undefined) {
+                return false
+            }
+            const filtered = all.filter(c => c.id !== id)
             await fs.promises.writeFile(this.ruta, JSON.stringify(filtered, null, 2))
             return true
         } catch (err) {
--- src/routers/cart.js
+++ src/routers/cart.js
@@ -7,7 +7,7 @@ const routerCart = Router()
 const { CartModel } = require('../../models/cartModel')
 const { admin } = require('../Admin')
 
-const data = new CartModel('../cart.txt')
+const data = new CartModel('./cart.txt')
 
 
 
@@ -26,23 +26,28 @@ routerCart.post('/', async(re, resp) => {
 //b. DELETE: '/:id' - Vacía un carrito y lo elimina. 
 routerCart.delete('/:id', async(req, resp) => {
     let check = false
-    const id = number(req.params.id)
+    const id = parseInt(req.params.id)
     try {
-        check = await data.delete()
+        check = await data.delete(id)
         if (check) {
             resp.json(`El carrito id: ${id} fue eliminado`)
-        } else
-            resp.json(`El carrito id: ${id} NO se pudo eliminar`)
+        } else {
+            resp.json(`El carrito id: ${id} NO se encontro`)
+        }
     } catch (err) {
         throw err
     }
 })
 
 //c. GET: '/:id/productos' - Me permite listar todos los productos guardados en el carrito 
-routerCart.get('/:id/productos', async(re, resp) => {
+routerCart.get('/:id/productos', async(req, resp) => {
+    const idCart = req.params.id
     try {
-        const all = await data.getAll()
-        resp.json(all)
+        const mycart = await data.getById(idCart)
+        if (mycart == null) {
+            resp.json('no se encontro el carrito')
+        }
+        resp.json(mycart.productos)
 
     } catch (err) {
         throw err
@@ -52,28 +57,45 @@ routerCart.get('/:id/productos', async(re, resp) => {
 //d. POST: '/:id/productos' - Para incorporar productos al carrito por su id de producto 
 routerCart.post('/:id/productos', async(req, resp) => {
 
-    let { precio, title, qty } = req.body
-    let idCart = parseInt(req.params.id)
-    let newProduct = {
+    const { price, title, qty } = req.body
+    const idCart = parseInt(req.params.id)
+    const newProduct = {
         "title": title,
         "price": price,
         "qty": qty
     }
 
     try {
-
-        let id = await data.save(newProduct)
-            //res.redirect('/api/productos')
-
+        const saved = await data.addProduct(newProduct, idCart)
+        if (!saved) {
+            resp.json(`eL ${idCart} no existe`)
+        }
+        resp.json("el producto se guardo con exito")
 
     } catch (err) {
+        resp.json("no se pudo guardar el producto")
         throw new Error
     }
 })
 
 //e. DELETE: '/:id/productos/:id_prod' - Eliminar un producto del carrito por su id de carrito y de producto
-routerCart.delete('/:id/productos/:id_prod', async(re, resp) => {
+routerCart.delete('/:id/productos/:id_prod', async(req, resp) => {
+    const idCart = parseInt(req.params.id)
+    const idProd = parseInt(req.params.id_prod)
+
     try {
+        const mycart = await data.getById(idCart)
+        if (mycart !== null) {
+            const deleted = await data.deleteProduct(idProd, mycart)
+            if (!deleted) {
+                resp.json("no se pudo eliminar el producto")
+            } else {
+                resp.json(mycart.productos, "producto eliminado")
+            }
+
+        }
+        resp.json('no se encontro el carrito')
+
 
     } catch (err) {
         throw err
